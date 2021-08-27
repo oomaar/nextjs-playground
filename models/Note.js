bf3213a7e541de9eb9e81b7c13656059169a99a9
Omar <omaar_5@hotmail.com>
2021-08-27 22:04:48 +0200

    setup: Note model

---
+++ models/Note.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const NoteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add a title !'],
+        unique: true,
+        trim: true,
+        maxlength: [40, 'Title can not be more than 40 characters']
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: [200, 'Description can not be more than 200 characters']
+    }
+});
+
+module.exports = mongoose.model.Note || mongoose.model('Note', NoteSchema);
