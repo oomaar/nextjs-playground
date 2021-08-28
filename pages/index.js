dac52b7fdbdaf563b218ec17cf159a9ae7024a88
Omar <omaar_5@hotmail.com>
2021-08-28 20:18:21 +0200

    render: HeadTag and GlobalStyle

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import { Navbar } from "../components";
+import { GlobalStyle, HeadTag } from "../Global";
 
 export default function Home() {
   return (
-    <>
+    <div>
+      <HeadTag />
+      <GlobalStyle />
+
       <Navbar />
-    </>
+    </div>
   );
 };
