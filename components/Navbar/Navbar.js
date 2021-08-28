47f3ecd0fc6e2e4b28fd8ff9409b625cb43a7537
Omar <omaar_5@hotmail.com>
2021-08-28 20:22:44 +0200

    complete: Navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import {
     Nav,
     Logo,
-    ButtonContainer,
     Button,
 } from "./styledNavbar";
 
 export const Navbar = () => {
     return (
-        <h1>
-            Test Navbar
-        </h1>
+        <Nav>
+            <Logo>NoteApp</Logo>
+            <Button href="#">Create Note</Button>
+        </Nav>
     );
 };
--- components/Navbar/styledNavbar.js
+++ components/Navbar/styledNavbar.js
@@ -1,9 +1,52 @@
 import styled from 'styled-components';
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  background-color: #0A81AB;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 2rem;
 
-export const Logo = styled.div``;
+  @media screen and (max-width: 280px) {
+    padding: 1rem 0.5rem;
+  }
 
-export const ButtonContainer = styled.div``;
+  @media screen and (max-width: 578px) {
+    padding: 1rem 1rem;
+  }
+`;
 
-export const Button = styled.a``;
+export const Logo = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 3rem;
+  cursor: pointer;
+
+  @media screen and (max-width: 280px) {
+    font-size: 1.5rem;
+  }
+
+  @media screen and (max-width: 567px) {
+    font-size: 1.8rem;
+  }
+`;
+
+export const Button = styled.a`
+  text-decoration: none;
+  color: #fff;
+  cursor: pointer;
+  font-size: 2rem;
+  
+  :hover {
+    text-decoration: underline;
+  }
+
+  @media screen and (max-width: 280px) {
+    font-size: 1.2rem;
+  }
+
+  @media screen and (max-width: 567px) {
+    font-size: 1.5rem;
+  }
+`;
