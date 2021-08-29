4c40a74e8548e6a11523500b880b5cfbd3215be4
Omar <omaar_5@hotmail.com>
2021-08-29 18:55:07 +0200

    style: font-family

--- Global/HeadTag/HeadTag.js
+++ Global/HeadTag/HeadTag.js
@@ -3,6 +3,11 @@ import Head from "next/head";
 export const HeadTag = () => {
     return (
         <Head>
+            {/* Google Font */}
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+
             <title>Note App</title>
         </Head>
     );
--- Global/styles/GlobalStyle.js
+++ Global/styles/GlobalStyle.js
@@ -13,5 +13,6 @@ export const GlobalStyle = createGlobalStyle`
     background-color: #f5f5f5;
     margin: 0;
     padding: 0;
+    font-family: 'Roboto', sans-serif;
   }
 `;
