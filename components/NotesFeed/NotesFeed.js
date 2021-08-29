0523c2c702e2c8212e5012cdf8b6f81a70618da9
Omar <omaar_5@hotmail.com>
2021-08-30 01:43:35 +0200

    refactor: NotesFeed

--- components/NotesFeed/NotesFeed.js
+++ components/NotesFeed/NotesFeed.js
@@ -1,30 +1,20 @@
-import Link from "next/link";
+import { Note } from "..";
 import {
     Container,
-    Card,
-    Title,
-    Description,
-    Button,
 } from "./styledNotesFeed";
 
 export const NotesFeed = ({ notes }) => {
-    console.log("🚀 ~ file: NotesFeed.js ~ line 11 ~ NotesFeed ~ notes", notes)
     return (
         <Container>
-            {notes.map(note => (
-                <Card key={note._id}>
-                    <Link href={`${note._id}`}>
-                        <Title>{note.title}</Title>
-                    </Link>
-                    <Description>{note.description}</Description>
-                    <Link href={`${note._id}`}>
-                        <Button>View</Button>
-                    </Link>
-                    <Link href={`${note._id}/edit`}>
-                        <Button>Edit</Button>
-                    </Link>
-                </Card>
+            {notes.map(({ _id, title, description }) => (
+                <Note
+                    key={_id}
+                    title={title}
+                    description={description}
+                    notes={notes}
+                />
             ))}
+
         </Container>
     );
 };
